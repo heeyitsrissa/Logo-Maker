733d46a8d3607f39c1130ec9472c1f7f56d8cf0e
Marissa Pratt <131078835+heeyitsrissa@users.noreply.github.com>
2024-02-25 15:12:43 -0500

    finshed index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const questions = [
     },
     {
         type: 'input',
-        name: 'text-color',
+        name: 'textColor',
         message: 'What color would you like your text to be?'
     },
     {
@@ -23,10 +23,45 @@ const questions = [
     },
     {
         type: 'input',
-        name: 'logo-color',
+        name: 'logoColor',
         message: 'What color would you like you logo to be?'
     }
 ]
 
+
+function generateSVG(data){
+    const { text, textColor, shape, logoColor } = data;
+    return `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
+
+    <${shape} cx="150" cy="100" r="80" fill="${logoColor}" />
+  
+    <text x="150" y="125" font-size="60" text-anchor="middle" fill="${textColor}">${text}</text>
+  
+  </svg>`
+}
 //write a function to write the SVG file names 'logo.svg'
 //when logo prints the text 'generated logo.svg' is console logged
+
+function writeToFile(data){
+    const fileName = `logo.svg`;
+    const svgContent = generateSVG(data);
+    fs.writeFile(fileName, svgContent, (err) =>{
+        if(err){
+            console.error(`Error writing file`, (err));
+        } else {
+            console.log(`Generated logo.svg`)
+        }
+    })
+}
+
+function init() {
+    inquirer.prompt(questions)
+    .then((answers) => {
+        writeToFile(answers);
+    })
+    .catch((error) => {
+        console.error(`Error`, error);
+    })
+}
+
+init();
